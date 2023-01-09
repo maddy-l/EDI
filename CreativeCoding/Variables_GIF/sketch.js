73d889cc8a7eb57d0984ce30ead9b7fd6c340ce7
maddy-l <lee.maddy@gmail.com>
2023-01-09 13:19:30 -0500

    graphic

--- CreativeCoding/Variables_GIF/sketch.js
+++ CreativeCoding/Variables_GIF/sketch.js
@@ -1,4 +1,9 @@
 //create a program that enables us to customize the number of squares in a grid
+let img;
+
+function preload (){
+  img = loadImage('Assets/Graphic.png');
+}
 
 function setup() {//runs once
  createCanvas (windowWidth, windowWidth); // creates a canvas 720x720 pixels
@@ -12,12 +17,10 @@ strokeWeight (1); //assigns stroke weight
 var num = 2; //number of sides in the array
 var sideLen = windowWidth/num; //side length variable
 
+
 for (var y = 0; y < windowWidth; y = y + sideLen) {
   for (var x = 0; x < windowWidth; x = x + sideLen) {
-  quad (x,y,
-        x + sideLen,y,
-        x + sideLen, sideLen + y,
-        x, sideLen + y);
+image (img, x, y, windowWidth/num, windowWidth/num);
   }
 }
 
