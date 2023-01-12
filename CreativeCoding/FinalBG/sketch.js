6d26bbe0962f130d1757deb322f6de6c05fa6c50
maddy-l <lee.maddy@gmail.com>
2023-01-12 09:00:43 -0500

    update

--- CreativeCoding/FinalBG/sketch.js
+++ CreativeCoding/FinalBG/sketch.js
@@ -17,13 +17,17 @@ strokeWeight (1); //assigns stroke weight
 var num = 5; //number of sides in the array
 var sideLen = windowWidth/num; //side length variable
 
+translate (-100,-100);// translation creates a bleed effect
 
-for (var y = 0; y < windowWidth; y = y + sideLen) {
-  for (var x = 0; x < windowWidth; x = x + sideLen) {
+for (var y = 0; y < 2 * windowWidth; y = y + sideLen) {
+  for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
 image (img, x, y, windowWidth/num, windowWidth/num);
   }
 }
 
 
 
+}
+function windowResized (){
+  resizeCanvas (windowWidth, windowHeight);
 }
