e94481e94e2f2c650e7a21756372d69e5506c10b
maddy-l <lee.maddy@gmail.com>
2023-01-12 09:48:27 -0500

    final

--- CreativeCoding/FinalBG/sketch.js
+++ CreativeCoding/FinalBG/sketch.js
@@ -1,16 +1,17 @@
 //create a program that enables us to customize the number of squares in a grid
 let img;
 
+
 function preload (){
   img = loadImage('Assets/Graphic1.png');
 }
 
 function setup() {//runs once
  createCanvas (windowWidth, windowWidth); // creates a canvas 720x720 pixels
- background (173, 216, 230); background color
 }
 
 function draw() {//runs in a loop
+  background (173, 216, 230); //background color
 fill ('turquoise'); //fills shape with RGB color
 strokeWeight (1); //assigns stroke weight
 
