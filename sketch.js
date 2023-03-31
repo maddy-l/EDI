d24581022812f84e6b2d5e0677144e91c351023b
maddy-l <113398672+maddy-l@users.noreply.github.com>
2023-03-31 08:08:20 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 //create a program that enables us to customize the number of squares in a grid
 let img;
-
-
 function preload (){
   img = loadImage('Assets/Graphic1.png');
 }
-
 function setup() {//runs once
  createCanvas (windowWidth, windowWidth); // creates a canvas 720x720 pixels
 }
@@ -15,10 +12,10 @@ function draw() {//runs in a loop
 fill ('turquoise'); //fills shape with RGB color
 strokeWeight (1); //assigns stroke weight
 
+translate (-100,-100);// translation creates a bleed effect
 var num = 5; //number of sides in the array
 var sideLen = windowWidth/num; //side length variable
 
-translate (-100,-100);// translation creates a bleed effect
 
 for (var y = 0; y < 2 * windowWidth; y = y + sideLen) {
   for (var x = 0; x < 2 * windowWidth; x = x + sideLen) {
